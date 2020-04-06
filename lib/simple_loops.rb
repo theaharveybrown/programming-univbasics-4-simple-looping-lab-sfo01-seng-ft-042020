19867884c91aca4890380737bbd8988401f25919
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-04-06 19:04:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,11 @@ def loop_message_five_times(message)
     count +=1 
   end 
 end 
+
+def loop_message_n_times(message, n)
+  count = 0 
+  while count < n do 
+    puts message
+    count +=1 
+  end 
+end 
